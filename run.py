0d5d6b854708c99c08e9ef73c421b3032e0c66bd
Julius Ware <jcware8@gmail.com>
2017-10-03 15:52:36 -0400

    Got routes working

--- run.py
+++ run.py
@@ -1,17 +1,17 @@
 from app.db import db, Profile
+from app.app import create_app
 from app.generate_jwt_token import generate_jwt_token
 from datetime import datetime
 from flask import Flask, redirect, make_response, request
-from integration_client.employee import employee as employee_client
 from jinja2 import Environment, FileSystemLoader
 from os import environ, path
 from time import mktime
 
-app = Flask(__name__)
+app = create_app()
 
 if __name__ == '__main__':
     # TODO: Remove when micro-services for Account exist
     app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('SQLALCHEMY_DATABASE_URI')
     db.init_app(app)
 
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5001)
